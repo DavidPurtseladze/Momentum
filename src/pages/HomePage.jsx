f357a47f515c302a8d24a57910cf71f3066928dc
Daviti Purtseladze <dphurtseladze@gmail.com>
2025-03-14 19:14:41 +0400

    Status Fetch Added, Tasks Fetch Added

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -3,11 +3,14 @@ import axios from 'axios';
 import Header from "../components/Header.jsx";
 import DropDown from "../components/DropDown.jsx";
 import Card from "../components/Card.jsx";
+import CheckBox from "../components/CheckBox.jsx";
 
 export default function () {
     const APIKEY = import.meta.env.VITE_API_KEY;
     const APIURL = import.meta.env.VITE_API_URL;
 
+    const [tasksData, setTasksData] = useState(null);
+    const [statusesData, setStatusesData] = useState(null);
     const [priorityData, setPriorityData] = useState(null);
     const [departmentsData, setDepartmentsData] = useState(null);
     const [employeeData, setEmployeeData] = useState(null);
@@ -15,6 +18,9 @@ export default function () {
     useEffect(() => {
         const fetchData = async () => {
             try {
+                const responseStatuses = await axios.get(APIURL + '/statuses');
+                setStatusesData(responseStatuses.data);
+
                 const responsePriorities = await axios.get(APIURL + '/priorities');
                 setPriorityData(responsePriorities.data);
 
@@ -26,6 +32,10 @@ export default function () {
                 });
                 setEmployeeData(responseEmployee.data);
 
+                const responseTasks = await axios.get(APIURL + '/tasks', {
+                    headers: { Authorization: `Bearer ${APIKEY}` },
+                });
+                setTasksData(responseTasks.data);
 
             } catch (err) {
 
@@ -52,34 +62,47 @@ export default function () {
                 </div>
 
                 <div className="grid grid-cols-4 gap-13">
-                    <div className="flex flex-col gap-[1.875rem]">
-                        <div className="w-full py-[0.9375rem] rounded-[0.625rem] bg-yellow">
-                            <h2 className="text-center text-xl font-medium text-white">დასაწყები</h2>
-                        </div>
+                    {/*<div className="flex flex-col gap-[1.875rem]">*/}
+                    {/*    <div className="w-full py-[0.9375rem] rounded-[0.625rem] bg-yellow">*/}
+                    {/*        <h2 className="text-center text-xl font-medium text-white">დასაწყები</h2>*/}
+                    {/*    </div>*/}
 
-                        <Card></Card>
-                    </div>
+                    {/*    <Card></Card>*/}
+                    {/*</div>*/}
 
-                    <div className="flex flex-col gap-[1.875rem]">
-                        <div className="w-full py-[0.9375rem] rounded-[0.625rem] bg-orange">
-                            <h2 className="text-center text-xl font-medium text-white">პროგრესში</h2>
-                        </div>
-                    </div>
+                    {/*<div className="flex flex-col gap-[1.875rem]">*/}
+                    {/*    <div className="w-full py-[0.9375rem] rounded-[0.625rem] bg-orange">*/}
+                    {/*        <h2 className="text-center text-xl font-medium text-white">პროგრესში</h2>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
 
-                    <div className="flex flex-col gap-[1.875rem]">
-                        <div className="w-full py-[0.9375rem] rounded-[0.625rem] bg-pink">
-                            <h2 className="text-center text-xl font-medium text-white">მზად ტესტირებისთვის</h2>
-                        </div>
-                    </div>
+                    {/*<div className="flex flex-col gap-[1.875rem]">*/}
+                    {/*    <div className="w-full py-[0.9375rem] rounded-[0.625rem] bg-pink">*/}
+                    {/*        <h2 className="text-center text-xl font-medium text-white">მზად ტესტირებისთვის</h2>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
 
-                    <div className="flex flex-col gap-[1.875rem]">
-                        <div className="w-full py-[0.9375rem] rounded-[0.625rem] bg-blue">
-                            <h2 className="text-center text-xl font-medium text-white">დასრულებული</h2>
+                    {/*<div className="flex flex-col gap-[1.875rem]">*/}
+                    {/*    <div className="w-full py-[0.9375rem] rounded-[0.625rem] bg-blue">*/}
+                    {/*        <h2 className="text-center text-xl font-medium text-white">დასრულებული</h2>*/}
+                    {/*    </div>*/}
+                    {/*</div>*/}
+
+                    {statusesData && statusesData.map((item, index) => (
+                        <div key={item.id} className="flex flex-col gap-[1.875rem]" data-id={item.id}>
+                            <div className="w-full py-[0.9375rem] rounded-[0.625rem] bg-blue">
+                                <h2 className="text-center text-xl font-medium text-white">{item.name}</h2>
+                            </div>
+                            {tasksData && tasksData
+                                .filter(task => task.status.id === item.id)
+                                .map((task, taskIndex) => (
+                                    <Card key={task.id} data={task}></Card>
+                                ))
+                            }
+
                         </div>
-                    </div>
+                    ))}
                 </div>
-
-
 
 
             </section>
