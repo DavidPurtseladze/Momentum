04962fc1b97f08f5486ab3471372a4a4caba11fd
Daviti Purtseladze <dphurtseladze@gmail.com>
2025-03-20 16:01:14 +0400

    Header HomePage Link Added

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 import logo from '../assets/logo.svg';
+import { Link } from "react-router-dom";
 
 export default function Header(props) {
     return (
         <header className="max-w-[105rem] w-full py-[1.875rem] mx-auto flex justify-between items-center">
-            <div>
+            <Link to="/">
                 <img src={logo} alt="Logo"/>
-            </div>
+            </Link>
+
             <div>
                 <button className="py-2.5 px-5 text-base text-primary-text border border-purple rounded-md mr-10" onClick={() => props.setOpenPopUp(true)}>თანამშრომლის შექმნა</button>
                 <button className="py-2.5 px-5 text-base bg-purple text-white rounded-md">+ შექმენი ახალი დავალება</button>
